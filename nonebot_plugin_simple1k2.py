651e41fa36d1e66c842f7b1eaa79f6ef2364ac2c
OREOCODEDEV <17620745509@126.COM>
2022-09-02 00:07:16 +0800

    改用kyaruutils插件

--- nonebot_plugin_simple1k2.py
+++ nonebot_plugin_simple1k2.py
@@ -1,8 +1,11 @@
 from nonebot.plugin.on import on_command
-from nonebot.params import CommandArg
-from nonebot.adapters import Message
+from nonebot.plugin.load import require
+from nonebot.params import Depends
+from typing import List
 
-helpText = "请输入：一穿二 剩余秒数 BOSS血量 目标补偿（非必需）\n如：“一穿二 34 2000”或“一穿二 34 2000 56”"
+require("nonebot_plugin_kyaruutils")
+from .nonebot_plugin_kyaruutils import ArgTools  # type:ignore
+
 simple_1k2_matcher = on_command("一穿二")
 
 
@@ -18,21 +21,11 @@ def calc(x: float, y: float, *args: float) -> float:
 
 
 @simple_1k2_matcher.handle()
-async def feedback(command_arg: Message = CommandArg()):
-    command_arg_list = command_arg.extract_plain_text().split()
+@ArgTools.filter_decorator(count=[2, 3], param_desp=["剩余时间", "BOSS血量", "期望时间"])
+async def feedback(command_arg_list: List[float] = Depends(ArgTools.extract_list(proc_list=[float]))):
     command_arg_length = len(command_arg_list)
-    try:
-        command_arg_list = list(map(lambda i: float(i), command_arg_list))
-    except ValueError:
-        await simple_1k2_matcher.finish("参数错误：参数转换至数字时发生错误")
-    if command_arg_length == 0:
-        await simple_1k2_matcher.finish(helpText)
-    if not 2 <= command_arg_length <= 3:
-        await simple_1k2_matcher.finish(helpText + "\n错误0")
-
     c1 = command_arg_list[0]
     c2 = command_arg_list[1]
-
     if c1 <= 0 or c2 <= 0 or c1 >= 90:
         await simple_1k2_matcher.finish("参数错误：超出范围")
     if c1 >= 35:
@@ -40,11 +33,11 @@ async def feedback(command_arg: Message = CommandArg()):
     if command_arg_length == 2:
         result = calc(c1, c2)
         needs = c2 - result
-    elif command_arg_length == 3:
+    else:
         c3 = command_arg_list[2]
         if not (c3 >= (c1 + 20) and c3 <= 90):
             await simple_1k2_matcher.finish("目标补偿时间错误，必须不小于剩余时间+20s且不大于90s")
         result = calc(c1, c2, c3)
         needs = c2 - result
-    reply = "若要按需求完成一穿二，应至少填{:.2f}伤害，使当前BOSS血量降至{:.2f}以下。".format(needs, result)
+    reply = "至少需填补%.2f伤害\n使当前BOSS血量降至%.2f以下" % (needs, result)
     await simple_1k2_matcher.finish(reply)
